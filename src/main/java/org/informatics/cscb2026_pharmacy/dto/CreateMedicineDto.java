ddcee32c31c3edafbca57943a7c43ca29eca6b9b
hkostadinova <hkostadinova@gmail.com>
2026-03-18 16:11:41 +0200

    Medicine DTO models added

---
+++ src/main/java/org/informatics/cscb2026_pharmacy/dto/CreateMedicineDto.java
@@ -0,0 +1,22 @@
+package org.informatics.cscb2026_pharmacy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateMedicineDto {
+    private String name;
+
+    private BigDecimal price;
+
+    private LocalDate expirationDate;
+}
+
---
+++ src/main/java/org/informatics/cscb2026_pharmacy/dto/RecipeDto.java
@@ -0,0 +1,11 @@
+package org.informatics.cscb2026_pharmacy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RecipeDto {
+    private long id;
+    private String number;
+}
